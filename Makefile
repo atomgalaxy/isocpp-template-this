cd11dbdd256bac38b92a3172318a5f2c9eb847e7
Gašper Ažman <gasper.azman@gmail.com>
2017-10-24 11:32:06 +0100

    Fixed implementation, but std::forward seems to have some weird cases.

--- Makefile
+++ Makefile
@@ -1,10 +1,61 @@
+# turn off built-in rules
+.SUFFIXES:
+MAKEFLAGS += -r
+
+CXXFLAGS=-std=c++17
+CXX=clang++
+RUN_CXX=$(CXX) $(CXXFLAGS)
+
+.PHONY: all
 all:
-	clang++ -std=c++17 ./forward_like.cpp -o forward_like
-	! clang++ -std=c++17 \
-		-DCOMPILE_FAILING \
-		-o forward_like_should_fail \
-		./forward_like.cpp
-	echo "Success! Those errors are supposed to be there."
+
+all: template-this.pdf
+template-this.pdf: template-this.latex
+	pdflatex "$<"
+	pdflatex "$<"
+	pdflatex "$<"
+
+all: forward_like
+forward_like: forward_like.cpp
+	$(RUN_CXX) -o "$@" "$<"
+
+all: test-failing
+.PHONY: test-failing
+test-failing: 
+
+define test_failing_tpl
+.PHONY: test-forward-like-$1
+test-failing: test-forward-like-$1
+test-forward-like-$1: forward_like.cpp
+	( ! $$(RUN_CXX) -o "$$@" "$$<" -DENSURE_FAILS_TO_COMPILE_$1 \
+	> /dev/null 2>&1 ) || ( echo "$$@ succeeded, something is wrong." && false )
+endef
+def-test-failing=$(eval $(call test_failing_tpl,$1))
+
+$(call def-test-failing,1)
+$(call def-test-failing,2)
+$(call def-test-failing,3)
+$(call def-test-failing,4)
+$(call def-test-failing,5)
+$(call def-test-failing,6)
+$(call def-test-failing,7)
+$(call def-test-failing,8)
+$(call def-test-failing,9)
+$(call def-test-failing,10)
+$(call def-test-failing,11)
+$(call def-test-failing,12)
+$(call def-test-failing,13)
+$(call def-test-failing,14)
+$(call def-test-failing,15)
+$(call def-test-failing,16)
+$(call def-test-failing,17)
+$(call def-test-failing,18)
+$(call def-test-failing,19)
+$(call def-test-failing,20)
+$(call def-test-failing,21)
+$(call def-test-failing,22)
 
 clean:
 	rm forward_like
+
+
--- forward_like.cpp
+++ forward_like.cpp
@@ -1,86 +1,183 @@
 
 #include <iostream>
-#include <type_traits>
 #include <vector>
 
+#include <type_traits>
 
 // given a facility that transfers cv-qualifiers from type to type
+// START_LIKE_DEF
 template <typename From, typename To>
 class like {
-    template <bool Condition, template <typename> class Function, typename T>
-    using apply_if = std::conditional_t<Condition, Function<T>, T>;
-    using base = std::remove_cv_t<std::remove_reference_t<To>>;
-    using base_from = std::remove_reference_t<From>;
+  template <bool Condition, template <typename> class Function, typename T>
+  using apply_if = std::conditional_t<Condition, Function<T>, T>;
+  using base = std::remove_cv_t<std::remove_reference_t<To>>;
+  using base_from = std::remove_reference_t<From>;
 
-    static constexpr bool rv = std::is_rvalue_reference_v<From>;
-    static constexpr bool lv = std::is_lvalue_reference_v<From>;
-    static constexpr bool c = std::is_const_v<base_from>;
-    static constexpr bool v = std::is_volatile_v<base_from>;
+  static constexpr bool rv = std::is_rvalue_reference_v<From>;
+  static constexpr bool lv = std::is_lvalue_reference_v<From>;
+  static constexpr bool c = std::is_const_v<base_from>;
+  static constexpr bool v = std::is_volatile_v<base_from>;
 
 public:
-    using type = apply_if<lv, std::add_lvalue_reference_t,
-                 apply_if<rv, std::add_rvalue_reference_t,
-                 apply_if<v, std::add_volatile_t,
-                 apply_if<c, std::add_const_t,
-                 base>>>>;
+  using type = apply_if<lv, std::add_lvalue_reference_t,
+      apply_if<rv, std::add_rvalue_reference_t,
+          apply_if<c, std::add_const_t, apply_if<v, std::add_volatile_t, base>>>>;
 };
 
 template <typename From, typename To>
 using like_t = typename like<From, To>::type;
-
-// examples (and tests)
-static_assert(std::is_same_v<int, like_t<long, int const&&>>);
-static_assert(std::is_same_v<int&, like_t<long&, int const&&>>);
-static_assert(std::is_same_v<int&&, like_t<long&&, int const&&>>);
-static_assert(std::is_same_v<int const, like_t<long const, int>>);
-static_assert(std::is_same_v<int const&, like_t<long const&, int>>);
-static_assert(std::is_same_v<int const&&, like_t<long const&&, int>>);
-static_assert(std::is_same_v<int volatile const, like_t<long volatile const, int>>);
-static_assert(std::is_same_v<int volatile const&, like_t<long volatile const&, int>>);
-static_assert(std::is_same_v<int volatile const&&, like_t<long volatile const&&, int>>);
-static_assert(std::is_same_v<int volatile, like_t<long volatile, int>>);
-static_assert(std::is_same_v<int volatile&, like_t<long volatile&, int>>);
-static_assert(std::is_same_v<int volatile&&, like_t<long volatile&&, int>>);
+// END_LIKE_DEF
 
 // ... we can define forward_like like so:
+// START_FWD_LIKE_DEF
 template <typename Like, typename T>
-constexpr decltype(auto) forward_like(T& t) noexcept
-{
-    return static_cast<like_t<Like&&, T>>(t);
-}
-
-template <typename Like, typename T,
-    typename = std::enable_if_t<!(std::is_lvalue_reference_v<Like&&> && std::is_rvalue_reference_v<T&&>)>>
 constexpr decltype(auto) forward_like(T&& t) noexcept
 {
-    static_assert(!(std::is_lvalue_reference_v<Like&&> && std::is_rvalue_reference_v<T&&>),
-        "Trying to convert a rvalue to a lvalue is forbidden.");
-    return static_cast<like_t<Like&&, T>>(t);
+  // first, get `t` back into the value category it was passed in
+  // then, forward it as `Like` was passed in.
+  return std::forward<like_t<Like, T>>(std::forward<T>(t));
 }
+// END_FWD_LIKE_DEF
+
+template <typename T>
+struct empty {
+};
 
 // TEST for forward_like
+// START_FWD_LIKE_TEST_DEF
+template <typename T, typename U>
+using fwd_t = decltype(std::forward<T>(std::declval<U>()));
+template <typename T, typename U>
+using fwl_t = decltype(forward_like<T>(std::declval<U>()));
+template <typename T, typename U>
+auto inline static constexpr test_same_as_forward
+    = std::is_same_v<fwd_t<T, U>, fwl_t<T, U>>;
+// END_FWD_LIKE_TEST_DEF
+
 int main()
 {
-    {
-        static_assert(std::is_same_v<int&, decltype(forward_like<long&>(std::declval<int&>()))>);
-        auto x = std::vector<int>{ 1, 2, 3 };
-        forward_like<int&>(x);
-    }
-    {
-        static_assert(std::is_same_v<int&&, decltype(forward_like<long>(std::declval<int&>()))>);
-        auto x = std::vector<int>{ 1, 2, 3 };
-        forward_like<int&&>(x);
-    }
-    { // does not compile
-#ifdef COMPILE_FAILING
-        static_assert(std::is_same_v<int&, decltype(forward_like<long&>(std::declval<int&&>()))>, "should not compile");
-        forward_like<int&>(std::vector<int>{ 1, 2, 3 });
+  using std::declval;
+  using std::forward;
+  using std::is_same_v;
+  using std::vector;
+
+  // examples (and exhaustive tests)
+  // START_LIKE_EXAMPLES
+  static_assert(is_same_v<int, like_t<long, int const&&>>);
+  static_assert(is_same_v<int&, like_t<long&, int const&&>>);
+  static_assert(is_same_v<int&&, like_t<long&&, int const&&>>);
+  static_assert(is_same_v<int const, like_t<long const, int>>);
+  static_assert(is_same_v<int const&, like_t<long const&, int>>);
+  static_assert(is_same_v<int const&&, like_t<long const&&, int>>);
+  static_assert(is_same_v<int volatile const, like_t<long volatile const, int>>);
+  static_assert(is_same_v<int volatile const&, like_t<long volatile const&, int>>);
+  static_assert(is_same_v<int volatile const&&, like_t<long volatile const&&, int>>);
+  static_assert(is_same_v<int volatile, like_t<long volatile, int>>);
+  static_assert(is_same_v<int volatile&, like_t<long volatile&, int>>);
+  static_assert(is_same_v<int volatile&&, like_t<long volatile&&, int>>);
+  // END_LIKE_EXAMPLES
+
+  // START_FWD_LIKE_TEST
+  // std::forward<T> and std::forward_like<T> are the same
+  static_assert(test_same_as_forward<int, int&>);
+  static_assert(test_same_as_forward<int, int&&>);
+  static_assert(test_same_as_forward<int&&, int&>);
+  static_assert(test_same_as_forward<int&&, int&&>);
+  static_assert(test_same_as_forward<int&, int&>);
+#ifdef ENSURE_FAILS_TO_COMPILE_1 // fails.
+  forward<int&>(int());
 #endif
-    }
-    {
-        static_assert(std::is_same_v<int&&, decltype(forward_like<long>(std::declval<int&&>()))>);
-        forward_like<int&&>(std::vector<int>{ 1, 2, 3 });
-    }
+#ifdef ENSURE_FAILS_TO_COMPILE_2
+  forward_like<int&>(int());
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_3
+  empty<fwd_t<int&, int&&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_4 // this one succeeds too for above reasons
+  empty<fwl_t<int&, int&&>>{}; // check behavior
+#endif
+
+  static_assert(test_same_as_forward<int const, int&>);
+  static_assert(test_same_as_forward<int const, int&&>);
+  static_assert(test_same_as_forward<int const&&, int&>);
+  static_assert(test_same_as_forward<int const&&, int&&>);
+  static_assert(test_same_as_forward<int const&, int&>);
+#ifdef ENSURE_FAILS_TO_COMPILE_5 // this is binding a rvalue-ref to const&...
+  empty<fwd_t<int const&, int&&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_6
+  empty<fwl_t<int const&, int&&>>{}; // check behavior
+#endif
+
+#ifdef ENSURE_FAILS_TO_COMPILE_7
+  empty<fwd_t<int, int const&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_8
+  empty<fwl_t<int, int const&>>{}; // check behavior
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_9
+  empty<fwd_t<int, int const&&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_10
+  empty<fwl_t<int, int const&&>>{}; // check behavior
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_11
+  empty<fwd_t<int&&, int const&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_12
+  empty<fwl_t<int&&, int const&>>{}; // check behavior
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_13
+  empty<fwd_t<int&&, int const&&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_14
+  empty<fwl_t<int&&, int const&&>>{}; // check behavior
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_15
+  empty<fwd_t<int&, int const&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_16
+  empty<fwl_t<int&, int const&>>{}; // check behavior
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_17
+  empty<fwd_t<int&, int const&&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_18
+  empty<fwl_t<int&, int const&&>>{}; // check behavior
+#endif
+
+  static_assert(test_same_as_forward<int const, int const&>);
+  static_assert(test_same_as_forward<int const, int const&&>);
+  static_assert(test_same_as_forward<int const&&, int const&>);
+  static_assert(test_same_as_forward<int const&&, int const&&>);
+  static_assert(test_same_as_forward<int const&, int const&>);
+#ifdef ENSURE_FAILS_TO_COMPILE_19 // rvalue -> lvalue again
+  empty<fwd_t<int const&, int const&&>>{}; // check understanding
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_20
+  empty<fwl_t<int const&, int const&&>>{}; // check behavior
+#endif
+
+  static_assert(is_same_v<int&, fwl_t<long&, int&>>);
+  auto lvalue_vector = vector<int>{ 1, 2, 3 };
+  forward_like<int&>(lvalue_vector);
+
+  static_assert(is_same_v<int&&, fwl_t<long, int&>>);
+  forward_like<int&&>(lvalue_vector);
+
+#ifdef ENSURE_FAILS_TO_COMPILE_21
+  empty<fwl_t<long&, int&&>>;
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_22
+  empty<fwl_t<long&, int&&>>;
+#endif
+#ifdef ENSURE_FAILS_TO_COMPILE_23
+  forward_like<int&>(vector<int>{ 1, 2, 3 });
+#endif
+  static_assert(is_same_v<int&&, fwl_t<long, int&&>>);
+  forward_like<int&&>(vector<int>{ 1, 2, 3 });
+  forward_like<int>(vector<int>{ 1, 2, 3 });
+  // END_LIKE_TEST
 }
 
 // vim: textwidth=80
